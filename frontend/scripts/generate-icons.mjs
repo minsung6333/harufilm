299b650ee4207139dc57274b9cc67b142a572969
sms <sq15330@gmail.com>
2026-05-17 15:49:50 +0900

    feat: add app icons (192, 512 PNG) from SVG design

---
+++ frontend/scripts/generate-icons.mjs
@@ -0,0 +1,18 @@
+import sharp from "sharp";
+import { readFileSync } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const svgPath = join(__dirname, "../public/icons/icon.svg");
+const svg = readFileSync(svgPath);
+
+const sizes = [192, 512];
+
+for (const size of sizes) {
+  await sharp(svg)
+    .resize(size, size)
+    .png()
+    .toFile(join(__dirname, `../public/icons/icon-${size}.png`));
+  console.log(`Generated icon-${size}.png`);
+}
